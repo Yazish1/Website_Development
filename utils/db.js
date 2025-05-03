bcec04a1c29907073266cf699435c2077ce84422
Yazish1 <yazish87@gmail.com>
2025-05-03 20:39:05 +1000

    added info

---
+++ utils/db.js
@@ -0,0 +1,15 @@
+// utils/db.js
+
+import knex from 'knex';
+
+export const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: 5432,
+    ssl: { rejectUnauthorized: false },
+  },
+});
