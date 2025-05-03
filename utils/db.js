ca58dbfb9910ce99ec6b2e5d90a9f5e57f7eddab
Yazish1 <yazish87@gmail.com>
2025-05-03 21:03:06 +1000

    trying to fix

--- utils/db.js
+++ utils/db.js
@@ -3,13 +3,13 @@ import knex from 'knex';
 
 // Using environment variables to connect to the Neon database
 export const db = knex({
-  client: 'pg', // PostgreSQL client
+  client: 'pg',
   connection: {
-    host: process.env.PGHOST,         // The host from your .env
-    user: process.env.PGUSER,         // The user from your .env
-    password: process.env.PGPASSWORD, // The password from your .env
-    database: process.env.PGDATABASE, // The database name from your .env
-    ssl: { rejectUnauthorized: false }, // SSL is required for Neon connections
+    host: process.env.PGHOST,         // From .env (e.g., ep-still-bar-a76h9qn0.aws.neon.tech)
+    user: process.env.PGUSER,         // From .env (your Neon username)
+    password: process.env.PGPASSWORD, // From .env (your Neon password)
+    database: process.env.PGDATABASE, // From .env (your Neon database name)
+    ssl: { rejectUnauthorized: false }, // SSL required by Neon
   },
 });
 
